faeb1f860a3cd81812c92cc12fa757346f3e9ad2
zach <zobar291@learner.dvusd.org>
2023-01-04 08:26:57 -0700

    1st WPF app

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+    Private Const V As String = "Hello World"
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        TextBox1.Text = "Hello World"
+    End Sub
+End Class
